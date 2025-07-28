5db88973aadb1f8aace9c5a0b0f385a6b936042f
ayushkumarsingh121 <ayushkumarsinghrajput121@gmai.com>
2025-07-28 09:26:04 +0530

    Added Q2 of Variable in JavaScript

---
+++ Chapter_01_JavaScript_Basics/Variable in JavaScript/Q2.js
@@ -0,0 +1,25 @@
+/*
+
+### **2. Check Valid Variable Names**
+
+**🧠 Task**:
+Create variables with different valid names (`_name`, `$value`,). Assign values to them and print each one.
+
+✅ Focus: Naming rules
+
+```js
+// Example Output:
+// Alice
+// 100
+// ...
+```
+
+*/
+
+// SOLUTION
+
+let _name = "Ayush";
+let $value = 100
+
+console.log(_name)
+console.log($value)

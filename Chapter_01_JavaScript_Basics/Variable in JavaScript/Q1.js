3c3b76b923e8365c7fac90e1d4fa7d4add71d41e
ayushkumarsingh121 <ayushkumarsinghrajput121@gmai.com>
2025-07-28 09:24:25 +0530

    Added Q1 of Variable in JavaScript

---
+++ Chapter_01_JavaScript_Basics/Variable in JavaScript/Q1.js
@@ -0,0 +1,28 @@
+/*
+
+## ✅ **2 Easy Coding Questions**
+
+### **1. Declare and Print Variables**
+
+**🧠 Task**:
+Declare three variables using `var`, `let`, and `const`. Store any values and print them using `console.log`.
+
+✅ Focus: Syntax, declaration types
+
+```js
+// Expected Output (example):
+// 10 "Ayush" true
+```
+
+*/
+
+// SOLUTION
+
+var num = 10;
+console.log(num)
+
+let name = "Ayush";
+console.log(name)
+
+const value = true;
+console.log(value)

08efc099d01af45e2bf124d36ef5a539d20a92e7
ayushkumarsingh121 <ayushkumarsinghrajput121@gmai.com>
2025-07-28 09:07:46 +0530

    added Q1 of Introduction to JavaScript

---
+++ Chapter_01_JavaScript_Basics/Q1.js
@@ -0,0 +1,30 @@
+/* 
+
+## ✅ **2 Easy Coding Questions**
+
+### **1. Print Even Numbers (0 to 10)**
+
+**Difficulty**: Easy
+**Topic**: Loops, Operators
+
+🧠 *Task*: Write a program that prints all even numbers from 0 to 10 using a `for` loop.
+
+```js
+Expected Output:
+0
+2
+4
+6
+8
+10
+
+*/ 
+
+
+// SOLUTION
+
+for(let i=0; i<=10; i++){
+  if(i%2 == 0){
+    console.log(i)
+  }
+}

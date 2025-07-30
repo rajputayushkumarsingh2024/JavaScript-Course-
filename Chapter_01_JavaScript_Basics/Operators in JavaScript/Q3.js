e62c4c824fd72b6358890543782733206b57ea44
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-07-30 16:19:12 +0530

    Added Q3 of Operator in JavaScript

---
+++ Chapter_01_JavaScript_Basics/Operators in JavaScript/Q3.js
@@ -0,0 +1,28 @@
+/*
+
+### ✅ 3. **Comparison Operators**
+
+**Q:** Write a program that takes two values (`a = 10`, `b = "10"`) and checks:
+
+* `a == b`
+* `a === b`
+* `a != b`
+* `a !== b`
+
+📤 Output:
+
+a == b: true
+a === b: false
+a != b: false
+a !== b: true
+
+*/
+
+// SOLUTION
+
+let a = 10;
+let b = "10";
+console.log(`a == b : ${a == b}`)
+console.log(`a === b : ${a === b}`)
+console.log(`a != b : ${a != b}`)
+console.log(`a !== b : ${a !== b}`)

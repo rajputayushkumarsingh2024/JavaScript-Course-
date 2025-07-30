7de871ad6a2dc04d856ed914ccc1191d4cd98658
ayushkumarsingh121 <ayushkumarsinghrajput121@gmai.com>
2025-07-30 09:52:54 +0530

    Added Q1.js in Operations

---
+++ Chapter_01_JavaScript_Basics/Operators in JavaScript/Q1.js
@@ -0,0 +1,31 @@
+/*
+### ✅ 1. **Arithmetic Operators**
+
+**Q:** Write a program to take two numbers and print:
+
+* Their sum
+* Their difference
+* Their product
+* Their quotient
+* Their remainder
+
+📥 Example Input: `a = 15`, `b = 4`
+
+📤 Example Output:
+Sum: 19
+Difference: 11
+Product: 60
+Quotient: 3.75
+Remainder: 3
+*/
+
+// SOLUTION
+
+let a = 15;
+let b = 4;
+
+console.log("Sum:", a + b);
+console.log("Difference:", a - b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
+console.log("Remainder:", a % b);

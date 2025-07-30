1b0ae2324122ba5d0759f6fdcc7e996455b9f548
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-07-30 16:28:02 +0530

    Added Q5 of Operator in JavaScript

---
+++ Chapter_01_JavaScript_Basics/Operators in JavaScript/Q5.js
@@ -0,0 +1,15 @@
+/*
+### ✅ 5. **Ternary Operator**
+
+**Q:** Write a program that takes a number as input and checks if it's even or odd using the ternary operator.
+
+📥 Input: `7`
+📤 Output: `Odd`
+
+*/
+
+// SOLUTION 
+
+let num = 7;
+let result = (num % 2 === 0) ? "Even" : "Odd" 
+console.log(result)

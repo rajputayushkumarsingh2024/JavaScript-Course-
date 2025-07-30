15d892f7ea6905ab179df5f442eede55a9f73b10
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-07-30 16:23:08 +0530

    Added Q4 of Operator in JavaScript

---
+++ Chapter_01_JavaScript_Basics/Operators in JavaScript/Q4.js
@@ -0,0 +1,41 @@
+/*
+
+### ✅ 4. **Logical Operators**
+
+**Q:** Given the variables:
+
+```js
+let x = true;
+let y = false;
+```
+
+Print the result of:
+
+* `x && y`
+* `x || y`
+* `!x`
+* `!y`
+
+---
+
+*/
+
+// SOLUTION
+
+let x = true;
+let y = false;
+
+console.log(x && y)
+console.log(x || y)
+console.log(!x)
+console.log(!y)
+
+
+
+
+
+
+
+
+
+

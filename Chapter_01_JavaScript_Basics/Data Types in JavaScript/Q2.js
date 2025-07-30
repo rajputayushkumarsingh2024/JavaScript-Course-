229d0355329281534a320f22e2a905008f2294a0
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-07-30 16:50:36 +0530

    Added Q2.js of Datatype

---
+++ Chapter_01_JavaScript_Basics/Data Types in JavaScript/Q2.js
@@ -0,0 +1,28 @@
+/*
+
+### 🟢 Q2. Undefined or Null?
+
+Write a program that declares one variable with no value and one variable with `null`. Print both and explain the difference in the output.
+
+📤 Example Output:
+
+```
+undefined  
+null
+```
+
+---
+
+*/
+
+// SOLUTION 
+
+let age; 
+let name = null
+console.log(name)
+console.log(age)
+
+// Explanation:
+// let age; → No value assigned → undefined
+// let name = null; → Explicitly assigned to mean "no value"
+

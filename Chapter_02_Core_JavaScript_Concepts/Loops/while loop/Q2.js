1072ab95e37bb1cadb20ce2fa6e6795caf7f5814
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-01 16:12:14 +0530

    Added Q2 in While loops

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/while loop/Q2.js
@@ -0,0 +1,23 @@
+/*
+**Q2. Print Even Numbers from 2 to 10**
+
+**🧠 Task:**
+Use a `while` loop to print even numbers from 2 to 10.
+
+✅ Output:
+
+2  
+4  
+6  
+8  
+10
+
+*/
+
+let i = 1;
+while(i<=10){
+  if(i%2==0){
+    console.log(i)
+  }
+  i++
+}

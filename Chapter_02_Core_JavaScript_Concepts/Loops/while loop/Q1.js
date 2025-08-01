b10c0314650a5a9df81a223ea8c272848e9181ce
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-01 16:09:44 +0530

    Added Q1 in While loops

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/while loop/Q1.js
@@ -0,0 +1,21 @@
+/*
+
+### 🟢 **Easy Level**
+
+**Q1. Print First 5 Natural Numbers**
+Use a `while` loop to print the first 5 natural numbers (1 to 5).
+
+**✅ Output:**
+1  
+2  
+3  
+4  
+5
+
+*/
+
+let i = 1; 
+while(i<=5){
+  console.log(i);
+  i++
+}

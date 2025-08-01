fe5316cd982fb86b20aeb025b505f2051fd31e71
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-01 10:10:00 +0530

    Added Q1.js in Pattern-format-Question

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/Patterns-format-Questions/Q1.js
@@ -0,0 +1,18 @@
+/*
+## 🟢 **Level 1: Basic Patterns (Stars 🔹)**
+
+### 1. Solid Rectangle
+
+*****
+*****
+*****
+
+*/
+
+for(let i=1; i<=3; i++){
+  let bag = ""
+  for(let j=1; j<=5; j++){
+    bag = bag + "*"
+  }
+  console.log(bag)
+}

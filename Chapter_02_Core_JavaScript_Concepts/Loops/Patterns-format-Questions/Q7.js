68a1abeb9dfedb2914dbbda8482f761ee6d41bd2
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-01 12:19:14 +0530

    Added Q7.js in Pattern-format-Question

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/Patterns-format-Questions/Q7.js
@@ -0,0 +1,21 @@
+/*
+
+7. Inverted Half Pyramid Numbers
+
+1234
+123
+12
+1
+
+*/
+
+for(let i=4; i>=1; i--){
+  let bag = "";
+  for(let j=1; j<=i; j++){
+    bag += j
+  }
+  console.log(bag)
+}
+
+
+

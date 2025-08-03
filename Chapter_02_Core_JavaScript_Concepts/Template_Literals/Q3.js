b2934994a45d6c1ec1f9416c0ff9ba1786ff0335
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-03 23:39:31 +0530

    Added Q3.js of Template_literals

---
+++ Chapter_02_Core_JavaScript_Concepts/Template_Literals/Q3.js
@@ -0,0 +1,12 @@
+/*
+### **3. Student Report Card**
+
+🧠 **Task:**
+Create a function that accepts a student name, subject, and marks, and returns:
+👉 *"Student: Ayush | Subject: Math | Marks: 92"*
+
+*/ 
+
+function reportCard(name, subject, marks) {
+  return `Student: ${name} | Subject: ${subject} | Marks: ${marks}`;
+}

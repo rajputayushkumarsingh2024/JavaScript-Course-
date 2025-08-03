82baf8e79a19a7e4a5301ff7c3f7842f2a35ee67
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-03 23:41:33 +0530

    Added Q4.js of Template_literals

---
+++ Chapter_02_Core_JavaScript_Concepts/Template_Literals/Q4.js
@@ -0,0 +1,13 @@
+/*
+### **4. File Path Builder**
+
+🧠 **Task:**
+Create a function that constructs a file path from folder and filename:
+👉 *"C:/Users/Ayush/Documents/file.txt"*
+
+
+*/
+
+function filePath(user, folder, fileName) {
+  return `C:/Users/${user}/${folder}/${fileName}`;
+}

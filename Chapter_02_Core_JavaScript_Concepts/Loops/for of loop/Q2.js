afd12faa3189dd6284e196dafd2f8ec82195c422
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-04 12:23:12 +0530

    Added Q2 of FOR OF LOOP

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/for of loop/Q2.js
@@ -0,0 +1,31 @@
+/*
+### **Q2. Print Characters of a String**
+
+🧠 **Task:**
+Use a `for...of` loop to print each character of the string `"JavaScript"`.
+
+```js
+let str = "JavaScript";
+```
+
+**Expected Output:**
+
+```
+J  
+a  
+v  
+a  
+S  
+c  
+r  
+i  
+p  
+t
+```
+*/
+
+let str = "JavaScript";
+
+for(let index of str){
+  console.log(index)
+}

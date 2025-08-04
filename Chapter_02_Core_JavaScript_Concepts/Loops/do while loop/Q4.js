506811709a7e17e34df3f909d6b19914994e09a2
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-04 11:39:00 +0530

    Added Q4.js in DO WHILE LOOP

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/do while loop/Q4.js
@@ -0,0 +1,25 @@
+/*
+### **Q4. Count Down from 7 to 1**
+
+**Task:**
+Use a `do-while` loop to print numbers from 7 to 1 in reverse.
+
+**Expected Output:**
+
+```
+7  
+6  
+5  
+4  
+3  
+2  
+1
+```
+*/
+
+let i = 7;
+do{
+  console.log(i)
+  i--
+}
+while(i>0)

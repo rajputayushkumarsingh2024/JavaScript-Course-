5ad037fc8b36f0d66793324e33676e7f6e51d4fe
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-04 11:31:09 +0530

    Added Q1.js in DO WHILE LOOP

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/do while loop/Q1.js
@@ -0,0 +1,23 @@
+/*
+### **Q1. Print Numbers from 1 to 5**
+
+**Task:**
+Use a `do-while` loop to print numbers from 1 to 5.
+
+**Expected Output:**
+
+```
+1  
+2  
+3  
+4  
+5
+```
+*/
+
+let i = 1;
+do{
+  console.log(i)
+  i++
+}
+while(i<6)

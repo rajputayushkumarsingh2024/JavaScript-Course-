b897382d1ff1d4018da589f6e6668360bf9da1d9
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-04 12:16:31 +0530

    Added Q1 of FOR OF LOOP

---
+++ Chapter_02_Core_JavaScript_Concepts/Loops/for of loop/Q1.js
@@ -0,0 +1,30 @@
+/*
+
+### **Q1. Print Each Name in an Array**
+
+🧠 **Task:**
+Given an array of names, print each name using a `for...of` loop.
+
+```js
+let names = ["Ayush", "Piyush", "Geetanjali", "Najuk"];
+```
+
+**Expected Output:**
+
+```
+Ayush  
+Piyush  
+Geetanjali  
+Najuk
+```
+
+---
+
+*/
+
+let names = ["Ayush", "Piyush", "Geetanjali", "Najuk"];
+
+for(let index of names){
+  console.log(index)
+}
+

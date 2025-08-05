5cd86ca3b9e147780017e0988e29aeeaeea3ce13
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-05 16:55:05 +0530

    Added Q7 of Slice()

---
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Slice()/Q7.js
@@ -0,0 +1,28 @@
+/*
+### ✅ Q2. Slice Without Affecting Length
+
+**Task:**
+Create a shallow copy using `slice()` of the first two elements in the array below.
+
+```js
+const arr = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 }
+];
+
+// Copy first two items only
+```
+
+*/
+
+const arr = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 }
+];
+
+console.log(arr)
+
+let copiedArr = arr.slice(0, 2);
+console.log(copiedArr)

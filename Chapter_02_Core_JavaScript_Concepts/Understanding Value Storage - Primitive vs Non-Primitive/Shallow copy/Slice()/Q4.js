d28e41508f99ae3a7ca4c65b753ec940738fb610
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-05 13:52:58 +0530

    Added Q4 of Slice()

---
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Slice()/Q4.js
@@ -0,0 +1,21 @@
+/*
+### 🔸 **Q2. Shallow Copy a Portion by Index**
+
+🧠 **Task:**
+Write a function `copyFromIndex` that takes an array, a `startIndex`, and an `endIndex`, and returns a **shallow copy** of that portion using `slice()`.
+
+```js
+// Example:
+copyFromIndex(['a', 'b', 'c', 'd', 'e'], 1, 4)
+// Output: ['b', 'c', 'd']
+```
+
+> ✅ `slice(start, end)` includes start index, but excludes end index.
+
+*/
+
+let copyFromIndex = (arr, startIndex, endIndex) => {
+  console.log([...arr].slice(startIndex, endIndex))
+}
+
+copyFromIndex(['a', 'b', 'c', 'd', 'e'], 1, 4)

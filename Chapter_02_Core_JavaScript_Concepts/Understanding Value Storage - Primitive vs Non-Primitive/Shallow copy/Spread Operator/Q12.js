32d48df5859becb26648c0d73b9edaf656dce061
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-05 12:39:08 +0530

    Added Q12.js in Spread Operator

---
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Spread Operator/Q12.js
@@ -0,0 +1,14 @@
+/*
+#### 🔹 Q1. Convert a String to an Array of Characters
+
+**Task:** Use the spread operator to split the string `"Hello"` into an array of individual characters.
+**Expected Output:** `['H', 'e', 'l', 'l', 'o']`
+
+```js
+let word = "Hello";
+// Your code here
+
+*/
+
+let word = "Hello";
+console.log([...word])

66516ab41ee29fb4df914509375b13d89190d308
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-05 10:53:32 +0530

    Added Q1.1.js in Spread Operator

---
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Spread Operator/Q1.1.js
@@ -0,0 +1,20 @@
+/*
+### 🔸 Q1. Merge Two Arrays
+
+**Task:**
+Merge the following arrays using the spread operator.
+
+```js
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+// Expected Output: [1, 2, 3, 4]
+```
+
+---
+*/
+
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+console.log([...arr1, ...arr2])

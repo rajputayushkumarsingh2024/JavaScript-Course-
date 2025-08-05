d45723de6ea923276c23818898d22bc331e8c1ed
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-05 12:17:44 +0530

    Added Q8.js in Spread Operator

--- Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Spread Operator/Q8.js
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Spread Operator/Q8.js
@@ -1,1 +1,18 @@
 /*
+### 🔸 Q8. Merge with Property Overwrite
+
+**Task:**
+Merge two objects using spread operator. The second object should overwrite a property from the first.
+
+```js
+let base = { name: "Ayush", role: "Student" };
+let update = { role: "Developer" };
+// Expected: { name: "Ayush", role: "Developer" }
+```
+
+*/
+
+let base = { name: "Ayush", role: "Student" };
+let update = { role: "Developer" };
+
+console.log({...base, ...update})

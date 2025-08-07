7d48d0284880a75d93115a390da99253535ef3a4
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-07 21:07:01 +0530

    Added Q3 of Other

---
+++ Other/Q3.js
@@ -0,0 +1,11 @@
+
+// ### ✅ **Q3. Add Two Numbers (Without Function)**
+
+// **🧠 Task:**
+// Add `a = 5` and `b = 3`, and store the result in `sum`.
+
+let a = 5;
+let b = 3;
+let sum = a + b;
+console.log(sum); 
+

51bd7b3f5d2cff789f712c6a54caf03f68d8bcae
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-07 21:09:38 +0530

    Added Q5 of Other

---
+++ Other/Q5.js
@@ -0,0 +1,14 @@
+
+// ### ✅ **Q5. If-Else Statement**
+
+// **🧠 Task:**
+// Write code that checks if a number is positive or negative.
+
+let number = -10;
+
+if (number > 0) {
+  console.log("Positive");
+} else {
+  console.log("Negative");
+}
+

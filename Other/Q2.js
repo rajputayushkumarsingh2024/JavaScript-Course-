194ba7436424101459e5fffc51e1646d49a679de
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-07 21:06:19 +0530

    Added Q2 of Other

---
+++ Other/Q2.js
@@ -0,0 +1,13 @@
+
+// ### ✅ **Q2. Data Type of a Value**
+
+// **🧠 Task:**
+// Write a function `getType` that returns the data type of a given value.
+
+
+function getType(value) {
+  return typeof value;
+}
+
+console.log(getType(42));       
+console.log(getType("hello"));  

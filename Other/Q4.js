d30b239b5a1dcf7bc1f45e16bfa1dcd8a4b2c8a4
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-07 21:07:50 +0530

    Added Q4 of Other

---
+++ Other/Q4.js
@@ -0,0 +1,9 @@
+
+// ### ✅ **Q4. Create and Access Array Elements**
+
+// **🧠 Task:**
+// Create an array of fruits and print the second fruit.
+
+
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits[1]); 

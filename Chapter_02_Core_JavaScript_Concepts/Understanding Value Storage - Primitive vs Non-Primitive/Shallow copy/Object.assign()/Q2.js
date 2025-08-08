677af315d0b69046464d2937b647099e0b249e58
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-08 09:30:15 +0530

    Update Q2.js of Object.assign( )

---
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Object.assign()/Q2.js
@@ -0,0 +1,24 @@
+/*
+🔸 Q1. Clone and Update Age
+🧠 Task:
+Write a function called cloneAndUpdateAge that takes an object with properties name and age as input.
+The function should return a new shallow-copied object where the age is updated to 21.
+Make sure the original object is not changed.
+
+📦 Example:
+cloneAndUpdateAge({ name: "Ayush", age: 19 });
+// Output: { name: "Ayush", age: 21 }
+
+*/
+
+let cloneAndUpdateAge = (obj) => {
+  let copiedObj = Object.assign({}, obj)
+  copiedObj.age = 21;
+  return copiedObj
+}
+
+let originalObject = { name: "Ayush", age: 19 }
+console.log("Original Object =", originalObject)
+
+let copiedObject = cloneAndUpdateAge(originalObject)
+console.log("Copied Object =", copiedObject)

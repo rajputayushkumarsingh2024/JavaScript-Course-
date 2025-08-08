89ea549b73289edcd96304e036e66e123981110e
Rajput Ayush Kumar Singh <rajputayushkumarsingh2024@gmail.com>
2025-08-08 09:41:20 +0530

    Added Q3.js of Object.assign( )

---
+++ Chapter_02_Core_JavaScript_Concepts/Understanding Value Storage - Primitive vs Non-Primitive/Shallow copy/Object.assign()/Q3.js
@@ -0,0 +1,25 @@
+/*
+### 🔸 **Q2. Clone and Add City**
+
+**🧠 Task:**
+Write a function named `cloneAndAddCity` that accepts an object with `name` and `age` properties.
+Return a new **shallow copy** of the object with an extra property: `city` set to `"Patna"`.
+The original object should stay the same.
+
+**📦 Example:**
+cloneAndAddCity({ name: "Ayush", age: 19 });
+// Output: { name: "Ayush", age: 19, city: "Patna" }
+
+*/
+
+let cloneAndAddCity = (obj) => {
+  let copiedObj = Object.assign({}, obj);
+  copiedObj.city = "Patna"
+  return copiedObj;
+}
+
+let originalObj = { name: "Ayush", age: 19 }
+console.log("Original Object = ", originalObj)
+
+let copiedObject = cloneAndAddCity(originalObj)
+console.log("Copied Object = ", copiedObject)
